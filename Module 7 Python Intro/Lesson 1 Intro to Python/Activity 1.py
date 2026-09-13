41a0ca832770c9b5a670cfc311b6dc61aa9d5020
Rudra Debnath <rudradebnath08@gmail.com>
2026-09-13 21:23:12 +0530

    Finished M7L1A1

---
+++ Module 7 Python Intro/Lesson 1 Intro to Python/Activity 1.py
@@ -0,0 +1,48 @@
+# Printing a greeting message
+print("Hello, WORLD!")
+
+# Printing my name
+print("My name is Rudra Debnath.")
+
+# Printing a fun fact about myself
+print("A fun fact about me is that I love to do animations.")
+
+# Performing and Printing a simple calculation
+print("The result of 5 + 3 is:", 5 + 3)
+
+name = "Rudra"
+print("My name is", name)
+
+print(f"My name is {name}")
+
+print(123)
+
+print("Hi\nBye")
+
+# Rule 1: You can't start a variable name with a number
+# Rule 2: You can only have letters, numbers and underscores in variable names.
+# Rule 3: You can't use Python keywords as variable names.
+
+import keyword
+print(keyword.kwlist)
+
+food = "Chicken Pizza"
+drink = "Coke"
+
+print("My favorite food is", food)
+print(f"My favorite drink is {drink}")
+
+print("123 * 678=", 123 * 678)
+
+print(f"15 * 1 = {15 * 1}")
+print(f"15 * 1 = {15 * 2}")
+print(f"15 * 1 = {15 * 3}")
+print(f"15 * 1 = {15 * 4}")
+
+a = "abcd"
+b = "efgh"
+
+#  String Concatenation - String Joining
+print(a + b)
+
+print("Hello" * 5)
