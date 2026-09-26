00305c456a4e6c50c4547224dae8687749b2a62b
Rudra Debnath <rudradebnath08@gmail.com>
2026-09-26 21:24:06 +0530

    Finished M7L4A1,A2,A3,A4

---
+++ Module 7 Python Intro/Lesson 4/Activity 1.py
@@ -0,0 +1,11 @@
+for i in range(1, 11):
+    print(i)
+
+name = "Rudra"
+
+for c in name:
+    print(c)
+
+
+for i in range(1, 11):
+    print(f"728 x {i} = {728 * i}")
---
+++ Module 7 Python Intro/Lesson 4/Activity 4.py
@@ -0,0 +1,12 @@
+# With a for loop, the number of repetitions is known beforehand.
+# With a while loop, the number of repitiions depends on the While Condition.
+
+i = 1
+sum = 0
+
+while i <= 10: 
+    sum = sum + i
+    i = i + 1
+
+print(sum)
+
